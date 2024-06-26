9830a70a6e91c0ee9d39df404ac6c47efca7b516
JoaoVieitas <vieitas@ua.pt>
2024-06-26 16:19:30 +0100

    fix  per-user access

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,8 +10,7 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
-    })
-    .authorization(allow => [allow.owner()]),
+    }).authorization(allow => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -20,10 +19,6 @@ export const data = defineData({
   schema,
   authorizationModes: {
     defaultAuthorizationMode: "userPool",
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
   },
 });
 
